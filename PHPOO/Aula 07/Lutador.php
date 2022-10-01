57f98aae2679162a3b99f14cd74d6be29ef4d740
katiabreu <abreu.katia@gmail.com>
2022-10-01 12:32:19 -0300

    site

---
+++ PHPOO/Aula 07/Lutador.php
@@ -0,0 +1,14 @@
+<?php
+class Lutador
+{
+    private $nome;
+    private $nacionalidade;
+    
+
+    public function __construct($property)
+    {
+        $this->property = $property;
+    }
+}
+  
+
